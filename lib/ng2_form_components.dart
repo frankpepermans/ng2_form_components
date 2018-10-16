27c479c29aa375d4ca0270a0da6cb0a8c80c4b5e
Igindo\FrankPepermans <frank@igindo.com>
2018-10-16 10:34:02 +0200

    adds ListItem cast

--- lib/ng2_form_components.dart
+++ lib/ng2_form_components.dart
@@ -1,70 +1,54 @@
 library ng2_form_components;
 
-export 'package:ng2_form_components/src/components/list_item.g.dart'
+import 'package:ng2_form_components/src/components/list_item.g.dart'
     show ListItem;
 
+export 'package:ng2_form_components/src/components/list_item.g.dart'
+    show ListItem;
 export 'src/components/internal/form_component.dart'
-    show LabelHandler, FormComponent, ResolveChildrenHandler, ResolveRendererHandler;
-
-export 'src/components/text_input.dart'
-    show TextInput, TextInputAction;
-
-export 'src/components/drop_down.dart'
-    show DropDown;
-
-export 'src/components/toaster.dart'
-    show Toaster, ToastMessageType;
-
-export 'src/components/auto_complete.dart'
-    show AutoComplete;
-
-export 'src/components/list_renderer.dart'
-    show ListRenderer, NgForTracker;
-
-export 'src/infrastructure/drag_drop_service.dart'
-    show DragDropService;
-
-export 'src/components/side_panel.dart'
-    show SidePanel;
-
-export 'src/components/window_container.dart'
-    show WindowContainer;
-
-export 'src/components/hierarchy.dart'
-    show Hierarchy, ShouldOpenDiffer;
-
+    show
+        LabelHandler,
+        FormComponent,
+        ResolveChildrenHandler,
+        ResolveRendererHandler;
+export 'src/components/text_input.dart' show TextInput, TextInputAction;
+export 'src/components/drop_down.dart' show DropDown;
+export 'src/components/toaster.dart' show Toaster, ToastMessageType;
+export 'src/components/auto_complete.dart' show AutoComplete;
+export 'src/components/list_renderer.dart' show ListRenderer, NgForTracker;
+export 'src/infrastructure/drag_drop_service.dart' show DragDropService;
+export 'src/components/side_panel.dart' show SidePanel;
+export 'src/components/window_container.dart' show WindowContainer;
+export 'src/components/hierarchy.dart' show Hierarchy, ShouldOpenDiffer;
 export 'src/components/item_renderers/default_list_item_renderer.dart'
     show DefaultListItemRenderer;
-
 export 'src/components/internal/list_item_renderer.dart'
-    show IsSelectedHandler, GetHierarchyOffsetHandler, ListDragDropHandler, ListDragDropHandlerType;
-
+    show
+        IsSelectedHandler,
+        GetHierarchyOffsetHandler,
+        ListDragDropHandler,
+        ListDragDropHandlerType;
 export 'src/components/item_renderers/default_hierarchy_list_item_renderer.dart'
     show DefaultHierarchyListItemRenderer;
-
-export 'src/components/animation/tween.dart'
-    show Tween;
-
-export 'src/components/helpers/drag_drop.dart'
-    show DragDrop;
-
-export 'src/components/form_input.dart'
-    show FormInput;
-
+export 'src/components/animation/tween.dart' show Tween;
+export 'src/components/helpers/drag_drop.dart' show DragDrop;
+export 'src/components/form_input.dart' show FormInput;
 export 'src/infrastructure/list_renderer_service.dart'
     show ListRendererService, ItemRendererEvent, ListRendererEvent;
-
-export 'src/infrastructure/hierarchy_level.g.dart'
-    show HierarchyLevel;
-
-export 'src/utils/window_listeners.dart'
-    show WindowListeners;
-
-export 'src/utils/mutation_observer_stream.dart'
-    show MutationObserverStream;
-
+export 'src/infrastructure/hierarchy_level.g.dart' show HierarchyLevel;
+export 'src/utils/window_listeners.dart' show WindowListeners;
+export 'src/utils/mutation_observer_stream.dart' show MutationObserverStream;
 export 'src/components/item_renderers/drop_effect_item_renderer.dart'
     show DropEffectItemRenderer;
+export 'src/utils/html_loader.dart' show HtmlLoader;
 
-export 'src/utils/html_loader.dart'
-    show HtmlLoader;
+ListItem<T> castListItem<T extends Comparable<dynamic>>(
+        ListItem<Comparable<dynamic>> listItem) =>
+    listItem != null
+        ? (new ListItem<T>()
+          ..container = listItem.container
+          ..data = listItem.data
+          ..isAlwaysOpen = listItem.isAlwaysOpen
+          ..parent = listItem.parent
+          ..selectable = listItem.selectable)
+        : null;
